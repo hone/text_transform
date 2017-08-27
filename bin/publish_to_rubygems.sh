c297c11b9dfa85ddcdf52bc83a9e6b73d5f9bbe2
Terence Lee <hone02@gmail.com>
2017-08-27 14:13:24 -0500

    more updates

--- bin/publish_to_rubygems.sh
+++ bin/publish_to_rubygems.sh
@@ -5,6 +5,6 @@ if [[ "$TRAVIS_TAG" =~ ^v[0-9.]+ ]]; then
   #chmod 0600 ~/.gem/credentials
 
   gem build text_transform.gemspec
-  gem unpack text_transform-`${TRAVIS_TAG#"v"}`.gem
-  tree text_transform-`${TRAVIS_TAG#"v"}`
+  gem unpack text_transform-*.gem
+  find text_transform-*
 fi

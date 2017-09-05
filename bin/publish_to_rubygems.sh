de24cf31a372a7f7d66211c043f2aafc9403bc8e
Terence Lee <hone02@gmail.com>
2017-09-05 14:25:52 -0500

    uncomment gem publish command

--- bin/publish_to_rubygems.sh
+++ bin/publish_to_rubygems.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ "$TRAVIS_TAG" =~ ^v[0-9.]+ ]]; then
-  #mkdir -p ~/.gem
-  #echo $RUBYGEMS_AUTH_TOKEN > ~/.gem/credentials
-  #chmod 0600 ~/.gem/credentials
+  mkdir -p ~/.gem
+  echo $RUBYGEMS_AUTH_TOKEN > ~/.gem/credentials
+  chmod 0600 ~/.gem/credentials
 
   gem build text_transform.gemspec
   gem unpack text_transform-*.gem
